b3589e7135ce443f3dabb85403ee2dcc0e7b65a4
PatrickMogul <patrick@easynode.one>
2022-05-05 15:29:19 -0500

    Better

--- pages/404.js
+++ pages/404.js
@@ -3,12 +3,14 @@ import 'nextra-theme-docs/style.css'
 import Link from 'next/link';
 
 export default function FourOhFour() {
-  return (<>
-    <h1>Sorry, we moved a LOT of things. 404 - Page Not Found</h1><br />
-    <Link href="/">
-      <a>
-        Click here to back home to guides.easynode.one
-      </a>
-    </Link>
-  </>)
+    return (<>
+        <div className="text-center">
+            <h1>Sorry, we moved a LOT of things. Page Not Found</h1><br />
+            <Link href="/">
+                <a>
+                    Click here to back home to guides.easynode.one
+                </a>
+            </Link>
+        </div>
+    </>)
 }
