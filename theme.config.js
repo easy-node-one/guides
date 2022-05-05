658bde3c2ac2448d9c46639741757e233cc75e50
PatrickMogul <patrick@easynode.one>
2022-05-05 13:16:50 -0500

    theme

--- theme.config.js
+++ theme.config.js
@@ -7,7 +7,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Easy Node</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        Validator Operator Companion Guide
+        Documentation Repository
       </span>
     </>
   ),
@@ -18,13 +18,13 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Easy Node Harmony ONE Validator Operator Companion Guide" />
-      <meta name="og:description" content="A guide by Easy Node to help people get started with Validating Harmony ONE Protocol" />
+      <meta name="description" content="Easy Node Documentation Repository" />
+      <meta name="og:description" content="The central location for all documentation guides created by Easy Node - Mostly focused on Harmony ONE Protocol Blockchain" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://easynode.one/static/img/ez_node_sq_lg.jpg" />
       <meta name="twitter:site:domain" content="companion.easynode.one" />
       <meta name="twitter:url" content="https://companion.easynode.one" />
-      <meta name="og:title" content="Easy Node: Harmony Validator Operator Companion Guide" />
+      <meta name="og:title" content="Easy Node: Documentation Repository" />
       <meta name="og:image" content="https://easynode.one/static/img/ez_node_sq_lg.jpg" />
       <meta name="apple-mobile-web-app-title" content="EasyNode" />
       <link
