2f5eaae778dbb0548f35801e0487529f63cf18d1
PatrickMogul <patrick@easynode.one>
2022-05-05 15:33:10 -0500

    2

--- pages/404.js
+++ pages/404.js
@@ -6,6 +6,8 @@ export default function FourOhFour() {
     return (<>
         <div className="justify-content-center text-center flex">
             <h1>Sorry, we moved a LOT of things. Page Not Found</h1><br />
+        </div>
+        <div className="justify-content-center text-center flex">
             <Link href="/">
                 <a>
                     Click here to back home to guides.easynode.one
