a82d4f400a862d93fdf74bbe5a97764b254babef
PatrickMogul <patrick@easynode.one>
2022-05-05 15:24:45 -0500

    idk

--- pages/404.js
+++ pages/404.js
@@ -2,12 +2,12 @@
 import Link from 'next/link';
 
 export default function FourOhFour() {
-  return <>
+  return (<>
     <h1>Sorry, we moved a LOT of things. 404 - Page Not Found</h1><br />
     <Link href="/">
       <a>
         Click here to back home to guides.easynode.one
       </a>
     </Link>
-  </>
+  </>)
 }
