d33f34b38655bc3c1fd87ee8c9e12dd151f33784
PatrickMogul <patrick@easynode.one>
2022-05-05 15:36:27 -0500

    try

--- pages/404.js
+++ pages/404.js
@@ -4,15 +4,16 @@ import Link from 'next/link';
 
 export default function FourOhFour() {
     return (<>
-        <div className="justify-content-center text-center flex">
-            <h1>Sorry, we moved a LOT of things. Page Not Found</h1><br />
-        </div>
-        <div className="justify-content-center text-center flex">
-            <Link href="/">
-                <a>
-                    Click here to back home to guides.easynode.one
-                </a>
-            </Link>
+        <div className="justify-content-center flex">
+            <p className="text-center ">
+                <h1>Sorry, we moved a LOT of things.</h1>
+                <h3>Page Not Found</h3>
+                <Link href="/">
+                    <a className="text-center">
+                        Click here to back home to guides.easynode.one
+                    </a>
+                </Link>
+            </p>
         </div>
     </>)
 }
