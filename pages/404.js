86ff193670b0b3e15051dc782615e519678591da
PatrickMogul <patrick@easynode.one>
2022-05-14 11:28:57 -0500

    Body

--- pages/404.js
+++ pages/404.js
@@ -4,16 +4,18 @@ import Link from 'next/link';
 
 export default function FourOhFour() {
     return (<>
-        <div class="justify-content-center flex">
-            <p class="text-center">
-                <h2>Sorry, we moved a LOT of things while combining all of our old guides into <a href="https://guides.harmony.one">guides.harmony.one</a> for everyone to enjoy in one spot.</h2>
-                <h3>Old Page Not Found - It's been moved to our new website.</h3><br /><br />
-                <Link href="/">
-                    <a>
-                        Click here to go to guides.easynode.one and search for your old content at it's new home.
-                    </a>
-                </Link>
-            </p>
-        </div>
+        <body>
+            <div class="justify-content-center flex">
+                <p class="text-center">
+                    <h2>Sorry, we moved a LOT of things while combining all of our old guides into <a href="https://guides.harmony.one">guides.harmony.one</a> for everyone to enjoy in one spot.</h2>
+                    <h3>Old Page Not Found - It's been moved to our new website.</h3><br /><br />
+                    <Link href="/">
+                        <a>
+                            Click here to go to guides.easynode.one and search for your old content at it's new home.
+                        </a>
+                    </Link>
+                </p>
+            </div>
+        </body>
     </>)
 }
