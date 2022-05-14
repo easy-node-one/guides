d380cc139c6133c3b53d0e17bde112bbd203ff80
PatrickMogul <patrick@easynode.one>
2022-05-14 11:26:19 -0500

    Better 404

--- pages/404.js
+++ pages/404.js
@@ -6,11 +6,11 @@ export default function FourOhFour() {
     return (<>
         <div class="justify-content-center flex">
             <p class="text-center">
-                <h1>Sorry, we moved a LOT of things.</h1>
-                <h3>Page Not Found</h3><br /><br />
+                <h2>Sorry, we moved a LOT of things while combining all of our old guides into <a href="https://guides.harmony.one">guides.harmony.one</a> for everyone to enjoy in one spot.</h2>
+                <h3>Old Page Not Found - It's been moved to our new website.</h3><br /><br />
                 <Link href="/">
                     <a>
-                        Click here to back home to guides.easynode.one
+                        Click here to go to guides.easynode.one and search for your old content at it's new home.
                     </a>
                 </Link>
             </p>
